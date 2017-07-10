1bdb72d367ca2d7f700c3a117a2c39ede4c3a365
DESKTOP-VJNO5T1\chiam <chiamcheng@gmail.com>
2017-07-10 16:42:41 +1000

    guards (canActivate, canActivateChild, canDeactivate) in conjunction with promises (es6) codes (learnt a bit), passing static and dynamic (resolver) data via app-routing module

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -8,23 +8,33 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { AuthGuard} from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ServerResolver  } from './servers/server/server-resolver.service';
 
 const appRouters:Routes = [
   { path: '', component: HomeComponent},
   { path: 'users', component: UsersComponent, children:[
     { path: ':id/:name', component: UserComponent}
   ]},
-  { path: 'servers', component: ServersComponent, children: [
-     { path: ':id', component: ServerComponent},
-     { path: ':id/edit', component: EditServerComponent}
+  { path: 'servers', //canActivate: [AuthGuard],
+    canActivateChild:[AuthGuard],
+    component: ServersComponent, children: [
+     { path: ':id', component: ServerComponent, resolve: {server:ServerResolver} },
+     { path: ':id/edit', component: EditServerComponent, canDeactivate:[ CanDeactivateGuard]}
   ]},
-  { path: 'not-found', component: PageNotFoundComponent},
+  //{ path: 'not-found', component: PageNotFoundComponent},
+  { path: 'not-found', component: ErrorPageComponent, data:{message:'Page not found'} },
   { path: '**', redirectTo:'/not-found'}
 ];
 
 @NgModule({
 	imports:[
-		RouterModule.forRoot(appRouters)
+  //# useHash is to route when web deployment in the real server to 404 or other pages
+  // it ignores the front part of # and parse things after the # like normal urls
+	// RouterModule.forRoot(appRouters, {useHash:true})
+    RouterModule.forRoot(appRouters)
 	],
 	exports:[RouterModule]
 })
--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute,Params,Router } from '@angular/router';
+import { ActivatedRoute,Params,Router, Data } from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -14,12 +14,18 @@ export class ServerComponent implements OnInit {
     , private router:Router) { }
 
   ngOnInit() {
-  	const id = +this.route.snapshot.params["id"]; // id = string here
+      this.route.data.subscribe(
+          (data: Data) => {
+            //the data["server"] variable is found and equals to the approuting module
+            this.server = data["server"];
+          }
+        );
+  	/*const id = +this.route.snapshot.params["id"]; // id = string here
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe(
     	(params:Params) => {
     		this.server = this.serversService.getServer(+params["id"]);
-    	})
+    	});*/
   }
 
   onEdit(){
